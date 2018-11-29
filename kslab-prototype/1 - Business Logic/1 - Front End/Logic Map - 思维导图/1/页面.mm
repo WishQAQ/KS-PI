c9a658116ecc6684700f4e439056088025b9a549
wish <wish-time@outlook.com>
2018-11-29 16:59:36 +0800

    原型

---
+++ kslab-prototype/1 - Business Logic/1 - Front End/Logic Map - 思维导图/1/页面.mm
@@ -0,0 +1,56 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1533882532030" ID="ID_538949383" MODIFIED="1534412421403" TEXT="&#x524d;&#x53f0;&#x9879;&#x76ee;&#x4e3b;&#x9875;" VGAP="2">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_538949383" ENDARROW="Default" ENDINCLINATION="-95;-63;" ID="Arrow_ID_29549715" SOURCE="ID_466028077" STARTARROW="None" STARTINCLINATION="-63;-6;"/>
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1534225770926" ID="ID_482564967" LINK="&#x6dd8;&#x8d27;.mm" MODIFIED="1534225770927" POSITION="right" TEXT="&#x6dd8;&#x8d27;"/>
+<node CREATED="1535966399472" ID="ID_1603032397" LINK="&#x670d;&#x52a1;.mm" MODIFIED="1535966399474" POSITION="right" TEXT="&#x670d;&#x52a1;"/>
+<node COLOR="#0033ff" CREATED="1534144653428" ID="ID_1109325454" LINK="&#x6d3b;&#x52a8;.mm" MODIFIED="1535966390171" POSITION="right" TEXT="&#x6d3b;&#x52a8;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#0033ff" CREATED="1533897481289" HGAP="-109" ID="ID_466028077" MODIFIED="1534144810660" POSITION="right" TEXT="&#x4e3b;&#x9875;" VSHIFT="-174">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<arrowlink DESTINATION="ID_538949383" ENDARROW="Default" ENDINCLINATION="-95;-63;" ID="Arrow_ID_29549715" STARTARROW="None" STARTINCLINATION="-63;-6;"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#0033ff" CREATED="1534144627413" HGAP="67" ID="ID_558211875" LINK="&#x767b;&#x5f55;&#x9875;&#x9762;.mm" MODIFIED="1534147764984" POSITION="right" TEXT="&#x767b;&#x5f55;&#x9875;&#x9762;" VSHIFT="-35">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node CREATED="1534144761681" ID="ID_1689648538" LINK="&#x641c;&#x7d22;.mm" MODIFIED="1534144810659" POSITION="right" TEXT="&#x641c;&#x7d22;">
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#0033ff" CREATED="1534142261312" ID="ID_663236173" LINK="&#x6c7d;&#x8f66;&#x3001;&#x6469;&#x6258;&#x8f66;&#x3001;&#x65e0;&#x4eba;&#x673a;&#x3001;&#x65e0;&#x4eba;&#x8f66;&#x677f;&#x5757;.mm" MODIFIED="1534144810657" POSITION="right" TEXT="&#x6c7d;&#x8f66;&#x3001;&#x6469;&#x6258;&#x8f66;&#x3001;&#x65e0;&#x4eba;&#x673a;&#x3001;&#x65e0;&#x4eba;&#x8f66;&#x677f;&#x5757;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#0033ff" CREATED="1534144613590" ID="ID_188437557" LINK="&#x5546;&#x57ce;.mm" MODIFIED="1534144810656" POSITION="right" TEXT="&#x5546;&#x57ce;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#0033ff" CREATED="1534144639571" ID="ID_208968998" LINK="&#x4e0a;&#x4f20;.mm" MODIFIED="1534147764984" POSITION="right" TEXT="&#x4e0a;&#x4f20;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#0033ff" CREATED="1534127788223" ID="ID_1891498217" MODIFIED="1534144810655" POSITION="left" TEXT="&#x5bf9;&#x8bdd;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#0033ff" CREATED="1534127794537" ID="ID_588422589" MODIFIED="1534144810655" POSITION="left" TEXT="&#x5ba2;&#x670d;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node COLOR="#0033ff" CREATED="1534127797120" ID="ID_527419667" MODIFIED="1534144810654" POSITION="left" TEXT="&#x7f6e;&#x9876;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="24"/>
+</node>
+<node CREATED="1534225791665" ID="ID_1379589089" MODIFIED="1534225801395" POSITION="left" TEXT="&#x4eba;&#x624d;"/>
+<node CREATED="1534225801991" ID="ID_1124170053" MODIFIED="1534225820194" POSITION="left" TEXT="&#x63d0;&#x51fa;&#x610f;&#x89c1;"/>
+<node CREATED="1534225820450" ID="ID_1627965236" MODIFIED="1534225823587" POSITION="left" TEXT="&#x4f01;&#x4e1a;&#x5408;&#x4f5c;">
+<node CREATED="1536127628985" ID="ID_1601714650" MODIFIED="1536127634356" TEXT="&#x4e0a;&#x4f20;&#x9644;&#x4ef6;"/>
+<node CREATED="1536127634863" ID="ID_1969859628" MODIFIED="1536127644688" TEXT="&#x8054;&#x7cfb;&#x4eba;&#x3001;&#x7535;&#x8bdd;"/>
+<node CREATED="1536127645432" ID="ID_301334712" MODIFIED="1536127654022" TEXT="&#x586b;&#x5199;&#x5177;&#x4f53;&#x5185;&#x5bb9;"/>
+</node>
+</node>
+</map>
